0b952c42af0ed3fbdcdcdbe92938c326817ba976
Brandon Bauley <bbauley@pdx.edu>
2018-07-28 11:28:11 -0700

    Reduced code for simplicity.

--- sieve/src/lib.rs
+++ sieve/src/lib.rs
@@ -1,12 +1,5 @@
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
     let mut result = Vec::new();
-    if upper_bound == 1 {
-        return result;
-    }
-    if upper_bound == 2 {
-        result.push(upper_bound);
-        return result;
-    }
     for num in 2..=upper_bound {
         let mut append = true;
         for element in result.iter() {
