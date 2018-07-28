e8178b26c10239c51f97fd63a4aa90ac26dc54e9
Brandon Bauley <bbauley@pdx.edu>
2018-07-28 11:27:11 -0700

    Passed 5 tests for sieve. Finished sieve assignment.

--- sieve/src/lib.rs
+++ sieve/src/lib.rs
@@ -1,3 +1,22 @@
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
-    unimplemented!("Construct a vector of all primes up to {}", upper_bound);
+    let mut result = Vec::new();
+    if upper_bound == 1 {
+        return result;
+    }
+    if upper_bound == 2 {
+        result.push(upper_bound);
+        return result;
+    }
+    for num in 2..=upper_bound {
+        let mut append = true;
+        for element in result.iter() {
+            if num % element == 0 {
+                append = false;
+            }
+        }
+        if append == true {
+            result.push(num);
+        }
+    }
+    result
 }
